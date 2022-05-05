cab42ed94483b4b70f081b7f71e083d590b80b28
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-05-05 13:17:21 +0530

    added live editor of html and css

---
+++ src/components/Window.js
@@ -0,0 +1,61 @@
+import React from "react";
+require('codemirror/lib/codemirror.css');
+require('codemirror/theme/material.css');
+require('codemirror/mode/xml/xml.js');
+require('codemirror/mode/javascript/javascript.js');
+
+class Window extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    }
+  }
+
+  componentDidMount() {
+    this.addStyle();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.htmlValue !== this.props.htmlValue) {
+      this.addStyle();
+    }
+
+    if (prevProps.cssValue !== this.props.cssValue) {
+      this.addStyle();
+    }
+  }
+
+  addStyle = () => {
+    var txt = `<head><style>${this.props.cssValue}</style></head><body class='output-text'>${this.props.htmlValue}<//body>`;
+    var frame = document.getElementById('output-text-holder');
+    frame = (frame.contentWindow || frame.contentDocument);
+    if (frame.document) frame = frame.document;
+    // console.log(frame.body);
+    // frame.body.innerHTML = txt;
+  
+    frame.open();
+    frame.write(txt);
+    frame.close();
+  }
+
+  render() {
+    return (
+        <div className="window">
+          <div className="output-header">
+            <div className="flex gap-half space-btw">
+              <div className="circle red"></div>
+              <div className="circle yellow"></div>
+              <div className="circle green"></div>
+            </div>
+            <h3 id="code-header">OUTPUT</h3>
+          </div>
+          <iframe title="output" id="output-text-holder">
+          </iframe>
+        </div>
+    )
+  }
+}
+
+export default Window;
+
---
+++ src/components/WithData.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {Controlled as CodeMirror} from 'react-codemirror2';
+
+require('codemirror/lib/codemirror.css');
+require('codemirror/theme/material.css');
+require('codemirror/mode/xml/xml.js');
+require('codemirror/mode/javascript/javascript.js');
+
+class WithData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    }
+  }
+
+  componentDidMount() {
+    this.setState({value: this.props.value})
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.value !== this.props.value) {
+      this.setState({value: this.props.value})
+    }
+  }
+
+  render() {
+    return (
+      <div id={this.props.language} >
+        <div className="header">
+          <div className="flex gap-half space-btw">
+            <div className="circle red"></div>
+            <div className="circle yellow"></div>
+            <div className="circle green"></div>
+          </div>
+          <h3 id="code-header">{this.props.header}</h3>
+        </div>
+        <CodeMirror
+          value={this.state.value}
+          options={{
+            mode: this.props.language,
+            theme: 'material',
+            lineNumbers: true,
+            addNew: true,
+            moveOnDrag:true,
+            smartIndent: true,
+            lineWrapping: true,
+            electricChars: true,
+          }}
+          onBeforeChange={(editor, data, value) => {
+            this.props.handleValue(value);
+          }}
+          onChange={(editor, value) => {
+          }}
+        />
+      </div>
+    )
+ }
+}
+
+export default WithData;
